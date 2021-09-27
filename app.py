a7e3202680f2250c583404ca0c3c9c5a28afd999
ABThalawaththa <aselb046@gmail.com>
2021-09-28 02:18:17 +0530

    initial test

---
+++ app.py
@@ -0,0 +1,22 @@
+#Core Pkgs
+import streamlit as st
+
+# EDA Pkgs
+import pandas as pd
+import numpy as np
+
+# Utils
+import os
+import joblib
+
+# Data Viz Pkgs
+import matplotlib.pyplot as plt
+import matplotlib
+
+
+def main():
+    st.title("Fuck you man")
+
+
+if __name__ == '__main__':
+    main()
